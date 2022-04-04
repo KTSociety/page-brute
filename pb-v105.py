4bef6601776a8289cf8fbd6ac15873770fdcacd6
Kryptox <102225606+KTSociety@users.noreply.github.com>
2022-04-04 08:15:45 +0200

    Update pb-v105.py

--- pb-v105.py
+++ pb-v105.py
@@ -76,7 +76,7 @@ def brutes(username, username_selector, password_selector, login_btn_selector, p
     optionss.add_argument('--disable-extensions')
     optionss.add_argument('--no-sandbox')
     browser = webdriver.Chrome(options=optionss)
-    wait = WebDriverWait(browser, 10)
+    wait = WebDriverWait(browser, 15)
     while True:
         try:
             for line in f:
@@ -95,11 +95,11 @@ def brutes(username, username_selector, password_selector, login_btn_selector, p
             print('CTRL+C - Operation aborted')
             exit()
         except selenium.common.exceptions.NoSuchElementException:
-            print ('AN ELEMENT HAS BEEN REMOVED FROM THE TARGETPAGE \nMAYBE THE PASSWORD WAS FOUND! \n')
+            print ('AN ELEMENT HAS BEEN REMOVED FROM THE TARGETPAGE \nMAYBE THE PASSWORD WAS FOUND! \n OR MAYBE YOU WERE BLOCKED \n')
             print ('Potencial Password has been found: {0}' .format(line))
             exit()
         except selenium.common.exceptions.TimeoutException:
-            print ('THERE IS A TIMEOUT ON TARGETPAGE, \nMAYBE THE PASSWORD WAS FOUND! \n')
+            print ('THERE IS A TIMEOUT ON TARGETPAGE, \nMAYBE THE PASSWORD WAS FOUND! \n OR MAYBE YOU WERE BLOCKED \n')
             print ('Potencial Password has been found: {0}' .format(line))
             exit()
 
